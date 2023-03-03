b61a6f685b0fe0f9baaae6eec8ab5a1e116ad285
Alejandro Gomez <alejandro@strike.me>
2023-03-03 12:28:08 +0100

    gradient

--- src/lib/Hexagon.js
+++ src/lib/Hexagon.js
@@ -1,9 +1,11 @@
 import "./Hexagon.css";
 import { Flex } from "@chakra-ui/react";
+import useColors from "./useColors";
 
 export default function Hexagon({ alt = "", picture, ...rest }) {
+  const { gradient } = useColors();
   return picture ? (
-    <Flex className="hex" {...rest}>
+    <Flex className="hex" background={gradient} {...rest}>
       <img alt={alt} src={picture} />
     </Flex>
   ) : null;
